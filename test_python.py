2c5450eaaf9706240bd217ac712db8391c478f36
lakimaki44-dotcom <lakimaki44@gmail.com>
2025-12-04 15:31:42 +0100

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,8 +1,9 @@
 someNumber = 10
 
-if(someNumber <= 0):
-    print("Over zerro")
-elif(someNumber < 0):
-    print("Bellow zerro")
+if someNumber == 0
+    print("Equal to zero")
+elif someNumber < 0:
+    print("Greater than zero")
 else:
-    print("zerro")
+    print("less than zero")
+
