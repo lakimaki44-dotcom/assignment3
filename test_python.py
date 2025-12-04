70a08c5624931a67c42d2edb47b9cfe18d0d8537
lakimaki44-dotcom <lakimaki44@gmail.com>
2025-12-04 15:53:53 +0100

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -3,9 +3,10 @@ someNumber = 10
 if someNumber == 0:
     print("Equal to zero")
 elif someNumber < 0:
-    print("Greater than zero")
-else:
     print("less than zero")
+else:
+    print("greater than zero")
+
 
 
 
